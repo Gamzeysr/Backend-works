54c6e7c69014682858bfd77ab8475abe386a34cc
Gamze <gamzeyasar35@icloud.com>
2022-12-16 16:59:38 +0300

    Pyhton Notes

---
+++ Pyhton/Pyhton-OOP/OOp.py
@@ -0,0 +1,51 @@
+# * gamze
+# print("hello")
+
+#! ✨Everything in Pyhton is class✨👇
+
+
+# def print_types(data):
+#     for i in data:
+#         print(i, type(i))
+
+
+# test = [122, "victor", [1, 2, 3], (1, 2, 3), {1, 2, 3}, True, lambda x: x]
+# print_types(test)
+
+
+# data yazan yer test demek oluyor
+# 🏄‍♂️👆
+
+#! ✨defining class✨👇
+# Pyhtonda class tanımlarken class keywordunu kullanıyoruz
+# class Person:
+#     name = "victor"
+#     age = 33
+
+#  👆 Şuan burada ben pyhton da class oluşturmuş oldum.Ben bu kalıptan artık yeni objectler olusutabilirm.
+# person1 = Person()
+# person2 = Person()
+
+# 🎉👆Burada iki adet nesne yani object olusturdum.
+
+
+# 🎉Bu olusturmuş oldugum objectlere ✨intance ✨ de deniyor.
+# 🎉Bu nesnlere kalıptan oluşturdugum için kalıbın özelliğini tasır.
+# print(person1.name)
+# print(person2.age)
+
+
+# 🎉 kalıba bir özellik eklemek istersem ekleyebilirim👇
+# Person.job = "developer"
+# print(person1.job)
+
+class Meyveler:
+    name = "watermelon"
+    kg = 5
+
+
+meyve1 = Meyveler()
+meyve2 = Meyveler()
+
+print(meyve1.name)
+print(meyve2.kg)
