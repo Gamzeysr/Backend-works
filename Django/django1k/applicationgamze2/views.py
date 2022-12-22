4ff8ab898de9643e8f087d597c914c76be3aec9b
Gamze <gamzeyasar35@icloud.com>
2022-12-22 22:58:03 +0300

    django1k

---
+++ Django/django1k/applicationgamze2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+
+ 
+# Create your views here.
+
+def gmz(request):
+    return HttpResponse("Hello Aplication2:))")
---
+++ Django/django1k/applicationgmz/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApplicationgmzConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "applicationgmz"
