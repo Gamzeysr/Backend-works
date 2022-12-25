1957d6a47510a645ed7db29b3dbb54c5d05ca4df
Gamze <gamzeyasar35@icloud.com>
2022-12-25 11:44:22 +0300

    views

--- WorkShop/001_WRKSHP/task/views.py
+++ WorkShop/001_WRKSHP/task/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse 
+from django.shortcuts import HttpResponse
 # Create your views here.
 
+
 def artist_list(request):
-    return HttpResponse ("welcome gamze")
+    return HttpResponse("welcome gamze")
