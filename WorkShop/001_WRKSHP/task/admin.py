a5b323548af382695ac9f493556f4bf374048618
Gamze <gamzeyasar35@icloud.com>
2022-12-25 11:50:24 +0300

    admin

--- WorkShop/001_WRKSHP/task/admin.py
+++ WorkShop/001_WRKSHP/task/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import(Artist,Album,Lyric,Song)
+from .models import (Artist, Album, Lyric, Song)
 
 # Register your models here.
 admin.site.register(Artist)
