04991932c342e0853fd8d7f82aba58d19c953dad
Gamze <gamzeyasar35@icloud.com>
2022-12-26 22:30:41 +0300

    models

--- Django/003_Django_Models2/fscohort/models.py
+++ Django/003_Django_Models2/fscohort/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50,blank=True)
+    last_name = models.CharField(max_length=30)
+    number = models.PositiveSmallIntegerField(blank=True)
+    
+
