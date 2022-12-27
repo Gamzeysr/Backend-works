daecf6a72e87d14ba8412c58e1453ab30d0caee9
Gamze <gamzeyasar35@icloud.com>
2022-12-27 17:18:30 +0300

    modelssettings

--- Django/003_Django_Models2/main/settings.py
+++ Django/003_Django_Models2/main/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 #    myapps
 'fscohort',
+'relatios',
 
 ]
 
--- Django/003_Django_Models2/relations/models.py
+++ Django/003_Django_Models2/relations/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.contrib.auth.models import User
+
+#👆 djangonun hali hazırda user modelini yaptık buraya 
 
 # Create your models here.
