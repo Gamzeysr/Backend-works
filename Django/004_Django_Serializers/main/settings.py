1e2673d8bf5068371195e8da859a8781e946e1fe
Gamze <gamzeyasar35@icloud.com>
2022-12-28 14:27:56 +0300

    keyS created

--- Django/004_Django_Serializers/main/settings.py
+++ Django/004_Django_Serializers/main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-nyjmn*8kfc2+in@$$gf*5t(i1r5#t4r6)pu$*gm@9z%g+x@j4m"
+SECRET_KEY = config("KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
