253917587d1fa6e00244c7b5b02ec02fe933a8c6
Gamze <gamzeyasar35@icloud.com>
2022-12-28 21:56:59 +0300

    urls int:pk

--- Django/004_Django_Serializers/student_api/urls.py
+++ Django/004_Django_Serializers/student_api/urls.py
@@ -4,6 +4,6 @@ from .views import home,student_api,student_api_get_update_delete
 urlpatterns = [
     path("",home),
     path("student/",student_api),
-    path("student/<int:pk",student_api_get_update_delete)
+    path("student/<int:pk>",student_api_get_update_delete)
 ]
 #*✨✨✨✨ 4.adım  olrak endpointlerımı olusturdum.
