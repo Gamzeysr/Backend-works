74102c1063a75a31b1d0517999fd50a4d52a8b18
Gamze <gamzeyasar35@icloud.com>
2022-12-28 12:37:10 +0300

    models

--- Django/003_Django_Models2/relations/models.py
+++ Django/003_Django_Models2/relations/models.py
@@ -1,5 +1,10 @@
-from django.contrib.auth.models import User
 
-#👆 djangonun hali hazırda user modelini yaptık buraya 
+from django.db import models
+from django.contrib.auth.models import User
+#! 👆 djangonun hali hazırda verdiği user modelini yaptık buraya 
 
 # Create your models here.
+class Profile(models.Model):
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to='profile',blank=True,null=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
