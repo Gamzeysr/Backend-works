7f43e896265dba4b9d1461e3ed5e73eaeaa7d3f0
Gamze <gamzeyasar35@icloud.com>
2022-12-28 13:22:06 +0300

    models

--- Django/003_Django_Models2/relations/admin.py
+++ Django/003_Django_Models2/relations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import(
+    Profile,
+    Address,
+    Product
+)
 # Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Address)
+admin.site.register(Product)
--- Django/003_Django_Models2/relations/models.py
+++ Django/003_Django_Models2/relations/models.py
@@ -8,3 +8,42 @@ class Profile(models.Model):
     bio = models.TextField(blank=True)
     image = models.ImageField(upload_to='profile',blank=True,null=True)
     user = models.OneToOneField(User,on_delete=models.CASCADE)
+
+    #! CASCADE'de mesela benim User'im silindiğinde bu User'a ait profile'in silinmesini istiyorsam CASCADE kullanıcam.
+
+  def __str__(self):
+        return self.user.username
+
+
+
+
+
+    '''
+on_delete properties:
+    # CASCADE -> if primary deleted, delete foreing too.
+    # SET_NULL -> if primary deleted, set foreign to NULL. (null=True)
+    # SET_DEFAULT -> if primary deleted, set foreing to DEFAULT value. (default='Value')
+    # DO_NOTHING -> if primary deleted, do nothing.
+    # PROTECT -> if foreign is exist, can not delete primary.
+    '''
+
+class Address(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=150)
+    city = models.CharField(max_length=20)
+    phone = models.CharField(max_length=20)
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+
+def __str__(self):
+        return f'{self.name} {self.user.username}'
+
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.name
