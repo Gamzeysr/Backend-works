ddcee33816c8d47d4e0a72202b8015591d611cb5
Gamze <gamzeyasar35@icloud.com>
2022-12-29 20:13:23 +0300

    serializers

--- Django/004_Django_Serializers/student_api/serializers.py
+++ Django/004_Django_Serializers/student_api/serializers.py
@@ -1,29 +1,41 @@
 from rest_framework import serializers
 from .models import Student
 
-#! Bunlar ilkel method👇
+# #! Bunlar ilkel method👇
 
-class StudentSerializer(serializers.Serializer):
+# class StudentSerializer(serializers.Serializer):
 
-    first_name = serializers.CharField(max_length=50)
-    last_name = serializers.CharField(max_length=50)
-    number = serializers.IntegerField()
-    age = serializers.IntegerField()
+#     first_name = serializers.CharField(max_length=50)
+#     last_name = serializers.CharField(max_length=50)
+#     number = serializers.IntegerField()
+#     age = serializers.IntegerField()
 
-#? Bu yukarıdaki serializers ı yazarsam asagıdaki iki methodu yazmak zorundayım 👇
-
-
-def create(self, validated_data):
-        return Student.objects.create(**validated_data)
+# #? Bu yukarıdaki serializers ı yazarsam asagıdaki iki methodu yazmak zorundayım 👇
 
 
-def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.number = validated_data.get('number', instance.number)
-        instance.age = validated_data.get('age', instance.age)
-        instance.save()
-        return instance
+# def create(self, validated_data):
+#         return Student.objects.create(**validated_data)
 
-        #! ilkel method buraya kadar👆
-    #*✨✨2.adım artık serializers ım hazır ben artık view.py e gidip serializers ımı import edebilirim.
+
+# def update(self, instance, validated_data):
+#         instance.first_name = validated_data.get('first_name', instance.first_name)
+#         instance.last_name = validated_data.get('last_name', instance.last_name)
+#         instance.number = validated_data.get('number', instance.number)
+#         instance.age = validated_data.get('age', instance.age)
+#         instance.save()
+#         return instance
+
+#         #! ilkel method buraya kadar👆
+#     #*✨✨2.adım artık serializers ım hazır ben artık view.py e gidip serializers ımı import edebilirim.
+
+
+class StudentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Student
+        fields = ["id","first_name","last_name","number","age"]
+
+
+
+
+
