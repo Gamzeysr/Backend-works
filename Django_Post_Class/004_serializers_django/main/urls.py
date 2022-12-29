ae38e0c10df326bc988d3cba11840cd342adffe6
Gamze <gamzeyasar35@icloud.com>
2022-12-29 09:34:47 +0300

    .

--- Django_Post_Class/004_serializers_django/main/urls.py
+++ Django_Post_Class/004_serializers_django/main/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/",include("student_api.url")),
+    path("api/",include("student_api.urls")),
 ]
