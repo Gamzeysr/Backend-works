b48c7ac0eb6a713e8e2655cf0ada19a6e907f260
Gamze <gamzeyasar35@icloud.com>
2023-01-02 17:33:42 +0300

    student-create/ urls

--- Django/005__Django__FBV/student_api/urls.py
+++ Django/005__Django__FBV/student_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import (
     home,
     students_list,
-    # student_create,
+    student_create,
     # student_detail,
     # student_update,
     # student_delete,
@@ -13,7 +13,7 @@ from .views import (
 urlpatterns = [
     path("", home),
     path("student-list/", students_list, name='list'),
-    # path("student-create/", student_create, name='create'),
+    path("student-create/", student_create, name='create'),
     # path("student-detail/<int:pk>/", student_detail, name='detail'),
     # path("student-update/<int:pk>/", student_update, name='update'),
     # path("student-delete/<int:pk>/", student_delete, name='delete'),
