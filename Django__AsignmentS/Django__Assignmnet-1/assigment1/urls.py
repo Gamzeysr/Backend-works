d38067adf29c23d43e8805b0c036858e40cefbea
Gamze <gamzeyasar35@icloud.com>
2023-01-05 22:45:46 +0300

    urls.py created

---
+++ Django__AsignmentS/Django__Assignmnet-1/assigment1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from.views import home,assigment1
+
+urlpatterns =[
+    path("",home),
+    path("assigment/",assigment1),
+]
+#  4.adım 
--- Django__AsignmentS/Django__Assignmnet-1/assigment1/views.py
+++ Django__AsignmentS/Django__Assignmnet-1/assigment1/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request):
     return HttpResponse('This is our home page....')
+
+    # 3.Adım
