037fc6884c24a480a0069dcb4cfe499edbdd0385
Gamze <gamzeyasar35@icloud.com>
2023-01-05 22:16:30 +0300

    models created

--- Django__AsignmentS/Django__Assignmnet-1/assigment1/models.py
+++ Django__AsignmentS/Django__Assignmnet-1/assigment1/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    username = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+    bio = models.TextField(blank=True,null=True)
+    register_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.username}'
+
+    class Meta:
+        ordering = ('username')
