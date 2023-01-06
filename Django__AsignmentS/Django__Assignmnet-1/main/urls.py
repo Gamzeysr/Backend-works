f3401c6bc731e0a7b0ebb5d1c397c296bc55c56d
Gamze <gamzeyasar35@icloud.com>
2023-01-06 12:32:42 +0300

    mainin içindeki urls

--- Django__AsignmentS/Django__Assignmnet-1/main/urls.py
+++ Django__AsignmentS/Django__Assignmnet-1/main/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,home
+from django.urls import path
+from userprofile.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("/",admin.site.home),
+    path("/",home),
 ]
+
+#! 2.adım
